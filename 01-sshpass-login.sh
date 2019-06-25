abf56ff5e590e446a8ee563f508f217ca46dad2b
yangye <ye.yang@deephi.tech>
2019-06-25 16:21:57 +0800

    add sshpass login script

--- 01-sshpass-login.sh
+++ 01-sshpass-login.sh
@@ -3,7 +3,6 @@
 ip=$1
 username=$2
 passwd=$3
-loop=0
 
 OSDIST=$(lsb_release -i |awk -F: '{print tolower($2)}' | tr -d ' \t')
 SSHPASS=sshpass
@@ -41,25 +40,29 @@ fi
 
 counter()
 {
-tput sc
 count=0;
+tput sc  #tput sc 存储光标位置
 while :;
 do
-    sleep 1
-    tput rc
-    tput ed
-    ((++loop))
-    printf "%s" $((count++))s;
-    [ $loop == 300 ] && echo -e "\ntried 300 times, ping failed, exit" && exit
+    if [ $count -lt 300 ];then
+        if ! ping -c 1 -n -w 1 "$ip" &> /dev/null
+        then
+            tput rc # tput rc 恢复光标位置
+            tput ed # tput ed 清除光标位置，到行尾的内容
+        else
+            break;
+        fi
+        printf "%s" $((++count))s;
+    else
+        echo -e "\ntried 300 times, ping failed, exit" 
+        exit 0
+    fi
 done
 }
 
 printf "%s" "Waiting for $ip ... "
 #while ! timeout 0.5 ping -c 1 -n "$1" &> /dev/null
-while ! ping -c 1 -n -w 1 "$ip" &> /dev/null
-do
-    counter
-done
+counter
 printf "\n%s\n" "$ip is online"
 
 echo "do command \"$SSHPASS -p $passwd ssh $2@$ip\""
